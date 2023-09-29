3044d82b5afadb32242200f31e5b741b398e3d3c
Andrei <z.andrei.83@mail.ru>
2023-09-29 13:17:16 +0200

    initial commit

---
+++ test_main_page.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+import warnings
+warnings.simplefilter("ignore", ResourceWarning)
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
