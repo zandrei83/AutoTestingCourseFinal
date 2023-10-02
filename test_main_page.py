dd0ee8e5b880afda478cadd8299e09af4b2b1193
Andrei <z.andrei.83@mail.ru>
2023-10-02 18:53:08 +0200

    implement test_guest_should_see_login_link test

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,19 @@
+import pytest
+
 from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 import warnings
 warnings.simplefilter("ignore", ResourceWarning)
 
-
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
