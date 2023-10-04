f035b2ebb583d40771ff527d5e53fd3df4377dc6
Andrei <z.andrei.83@mail.ru>
2023-10-04 23:17:31 +0200

    add need_reviews mark for 4 tests

--- test_product_page.py
+++ test_product_page.py
@@ -8,7 +8,7 @@ from .pages.login_page import LoginPage
 import warnings
 warnings.simplefilter("ignore", ResourceWarning)
 
-
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -74,6 +74,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     product_page.open()
     product_page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     product_page = ProductPage(browser, link)
@@ -81,6 +82,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
     product_page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     product_page = ProductPage(browser, link)
@@ -109,6 +111,7 @@ class TestUserAddToBasketFromProductPage:
 
         product_page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_208/'
         product_page = ProductPage(browser, link)
